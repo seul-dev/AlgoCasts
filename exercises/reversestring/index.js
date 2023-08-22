92b81da509917af539dc6d6f65d0d8881990b0ce
seul-dev <seul.dev@gmail.com>
2023-08-22 16:45:05 +0900

    reverseString solve

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return [...str].reduce((reversed, cur) => cur + reversed, '');
+}
 
 module.exports = reverse;
+
+// #1 solution
+//turn string into an array
+//call 'reverse()' method on the array
+//join the array back into a string
+//return the result
+
+// function reverse(str) {
+//   return [...str].reverse().join('');
+// }
+
+// #2 solution
+// create an empty string called 'reversed'
+// for each character in the provided string
+// take the character and add it to the start of 'reversed'
+// return the variable 'reversed'
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (const character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+//  #3 solution
+//
