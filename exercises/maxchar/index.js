2c0a5faf115946b64e32c2e048189766a8df19a2
seul-dev <seul.dev@gmail.com>
2023-08-23 13:07:03 +0900

    maxchar solve

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const map = new Map();
+  for (const char of str) {
+    const count = (map.get(char) || 0) + 1;
+    map.set(char, count);
+  }
+
+  let max = 0;
+  let maxChar = '';
+  map.forEach((count, char) => {
+    if (count > max) {
+      max = count;
+      maxChar = char;
+    }
+  });
+  return maxChar;
+}
 
 module.exports = maxChar;
