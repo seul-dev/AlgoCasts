1518ed67038824867085d10a9b5d09c08c467948
seul-dev <seul.dev@gmail.com>
2023-08-24 19:01:48 +0900

    solve capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str.replace(/\b(\w)/g, (match) => match.toUpperCase());
+}
+
+// /\b\w/g
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   return str
+//     .split(' ')
+//     .map((word) => word[0].toUpperCase() + word.slice(1))
+//     .join(' ');
+// }
