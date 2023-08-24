302cabfa5ac71949a232031ac5375dc61a86f478
seul-dev <seul.dev@gmail.com>
2023-08-24 23:55:05 +0900

    solve pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,58 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midPoint = Math.floor((2 * n - 1) / 2);
+  let add = '';
+  if (midPoint - row <= level.length && midPoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const totalWidth = 2 * n - 1;
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     const hashCount = 2 * row + 1;
+//     const startIdx = Math.floor((totalWidth - hashCount) / 2);
+//     for (let col = 0; col < totalWidth; col++) {
+//       if (col >= startIdx && col < startIdx + hashCount) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// function pyramid(n) {
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (col >= midPoint - row && col <= midPoint + row) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
