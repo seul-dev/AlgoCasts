f08f523a9713fd3ed1a304916cef61a56f2438f4
seul-dev <seul.dev@gmail.com>
2023-08-25 09:11:52 +0900

    solve vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return (str.match(/[aeiou]/gi) || []).length;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   const vowelSet = new Set(['a', 'e', 'i', 'o', 'u']);
+//   return [...str].filter((char) => vowelSet.has(char.toLowerCase())).length;
+// }
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (const char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
