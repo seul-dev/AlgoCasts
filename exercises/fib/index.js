155b9b6995491ecb17b27a8cb84e3b77cca1a151
seul-dev <seul.dev@gmail.com>
2023-08-26 11:01:31 +0900

    solve fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,66 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// function fib(n) {
+//   const memo = [0, 1];
+
+//   function fibHelper(n) {
+//     if (memo[n] !== undefined) {
+//       return memo[n];
+//     }
+
+//     memo[n] = fibHelper(n - 1) + fibHelper(n - 2);
+
+//     return memo[n];
+//   }
+
+//   return fibHelper(n);
+// }
+
+// function fib(n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+
+//   const memo = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     memo[i] = memo[i - 1] + memo[i - 2];
+//   }
+
+//   return memo[n];
+// }
